afec15090509e7194ecccc9f8857d9cb206975c8
Gustavo <gustavo@nixos.com>
2026-01-06 13:24:44 -0300

    feat(vim): change archive(task) to use <leader>pf

--- .config/nvim/lua/plugins/archive.lua
+++ .config/nvim/lua/plugins/archive.lua
@@ -1,9 +1,12 @@
 return {
   "Old-est/archive.nvim",
-  config = function ()
+  config = function()
     require("archive").setup()
-    -- TASK: oi
-    vim.keymap.set("n", "<A-t>", function() require("archive.task").create_task() end, { desc = "Create Task from Cursor" })
-    vim.keymap.set("n", "<leader>ft", function() return Snacks.picker.pick("task") end, { desc = "Find Tasks" })
-  end
+    vim.keymap.set("n", "<A-t>", function()
+      require("archive.task").create_task()
+    end, { desc = "Create Task from Cursor" })
+    vim.keymap.set("n", "<leader>pt", function()
+      return Snacks.picker.pick("task")
+    end, { desc = "Find Tasks" })
+  end,
 }
