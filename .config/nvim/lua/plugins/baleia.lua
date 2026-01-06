2ad0eaf2f43c35786d87d80ffac62fbf26e1c5b1
Gustavo <gustavo@nixos.com>
2026-01-06 13:02:48 -0300

    feat(vim): add baleia to handle git diff

---
+++ .config/nvim/lua/plugins/baleia.lua
@@ -0,0 +1,32 @@
+return {
+  "m00qek/baleia.nvim",
+  version = "*",
+  config = function()
+    vim.g.baleia = require("baleia").setup({})
+
+    -- Command to colorize the current buffer
+    vim.api.nvim_create_user_command("BaleiaColorize", function()
+      vim.g.baleia.once(vim.api.nvim_get_current_buf())
+    end, { bang = true })
+
+    -- Command to show logs
+    vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
+
+    vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+      pattern = "*.txt",
+      callback = function()
+        vim.g.baleia.automatically(vim.api.nvim_get_current_buf())
+      end,
+    })
+
+    vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+      pattern = "quickfix",
+      callback = function()
+        vim.api.nvim_set_option_value("modifiable", true, { buf = buffer })
+        vim.g.baleia.automatically(vim.api.nvim_get_current_buf())
+        vim.api.nvim_set_option_value("modified", false, { buf = buffer })
+        vim.api.nvim_set_option_value("modifiable", false, { buf = buffer })
+      end,
+    })
+  end,
+}
