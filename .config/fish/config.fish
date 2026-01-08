742736a4268520e239817c7a32c1b8ad111917cd
Gustavo <gustavo@nixos.com>
2026-01-08 15:06:37 -0300

    feat(fish): set vim keybinds in kitty, and improve zoxide change dir

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,9 +10,9 @@ function zoxide_cd
     # end
     #
     # if test $use_fuzzel -eq 1
-        set -l dir (zoxide query -l | fuzzel --dmenu)
+    set -l dir (zoxide query -l -s | fuzzel --dmenu --no-sort --match-mode=exact | awk "{print \$2}")
     # else
-        # set -l dir (zoxide query -l | fzf)
+    # set -l dir (zoxide query -l | fzf)
     # end
 
     if test -n "$dir"
@@ -22,53 +22,57 @@ function zoxide_cd
 end
 
 bind -a alt-z zoxide_cd
+bind -M insert alt-z zoxide_cd
+bind -M normal alt-z zoxide_cd
 
 # bind -a alt-z zi
 
 # # Set and configure VI Mode
-# fish_vi_key_bindings
-fish_default_key_bindings
-#
-# # Ctrl-A / Ctrl-E
-# bind -M insert \ca beginning-of-line
-# bind -M insert \ce end-of-line
-#
-# bind -M normal \ca beginning-of-line
-# bind -M normal \ce end-of-line
-#
-# # Kill / yank
-# bind -M insert \ck kill-line
-# bind -M insert \cy yank
-# bind -M insert \cw backward-kill-word
-# bind -M insert \cu kill-whole-line
-# bind -M normal \ck kill-line
-# bind -M normal \cy yank
-# bind -M normal \cw backward-kill-word
-# bind -M normal \cu kill-whole-line
-#
-# # Cursor movement
-# bind -M insert \cb backward-char
-# bind -M insert \cf forward-char
-# bind -M normal \cb backward-char
-# bind -M normal \cf forward-char
-#
-# bind -e \cp
-# bind -e \cn
-# bind -M normal \cp up-or-search
-# bind -M normal \cn down-or-search
-# bind -M insert \cp up-or-search
-# bind -M insert \cn down-or-search
-#
-# bind -M insert \cs search
-# bind -M normal \cs search
-#
-# bind -M insert \cz undo
-# bind -M insert \cZ redo
-# bind -M normal \cz undo
-# bind -M normal \cZ redo
+fish_vi_key_bindings
+# fish_default_key_bindings
+
+# Ctrl-A / Ctrl-E
+bind -M insert \ca beginning-of-line
+bind -M insert \ce end-of-line
+
+bind -M normal \ca beginning-of-line
+bind -M normal \ce end-of-line
+
+# Kill / yank
+bind -M insert \ck kill-line
+bind -M insert \cy yank
+bind -M insert \cw backward-kill-word
+bind -M insert \cu kill-whole-line
+bind -M normal \ck kill-line
+bind -M normal \cy yank
+bind -M normal \cw backward-kill-word
+bind -M normal \cu kill-whole-line
+
+# Cursor movement
+bind -M insert \cb backward-char
+bind -M insert \cf forward-char
+bind -M normal \cb backward-char
+bind -M normal \cf forward-char
+
+bind -e \cp
+bind -e \cn
+bind -M normal \cp up-or-search
+bind -M normal \cn down-or-search
+bind -M insert \cp up-or-search
+bind -M insert \cn down-or-search
+
+bind -M insert \cs search
+bind -M normal \cs search
+
+bind -M insert \cz undo
+bind -M insert \cZ redo
+bind -M normal \cz undo
+bind -M normal \cZ redo
 
 # Export nvim as manpager
-export MANPAGER='nvim -c "nmap <silent> q :q!<Cr>" +Man!' export PAGER='nvim -c "nmap <silent> q :q!<Cr>" -R'
+export MANPAGER='nvim -c "nmap <silent> q :q!<Cr>" +Man!'
+export PAGER='nvim -c "nmap <silent> q :q!<Cr>" +"BaleiaColorize" -R'
+
 # Export nvim as editor
 export EDITOR='nvim'
 
@@ -121,3 +125,36 @@ set -x GOBIN "$GOPATH/bin"
 # Append common directories for executable files to $PATH
 fish_add_path ~/.local/bin ~/.cargo/bin ~/Applications/depot_tools $HOME/.go/bin
 
+function fish_prompt --description 'Write out the prompt'
+    # Salva o último comando executado (por sessão)
+    set -gx NVIM_LAST_CMD (history --max=1)
+
+    set -l last_status $status
+    set -l normal (set_color normal)
+    set -l status_color (set_color brgreen)
+    set -l cwd_color (set_color $fish_color_cwd)
+    set -l vcs_color (set_color brpurple)
+    set -l prompt_status ""
+
+    # Since we display the prompt on a new line allow the directory names to be longer.
+    set -q fish_prompt_pwd_dir_length
+    or set -lx fish_prompt_pwd_dir_length 0
+
+    # Color the prompt differently when we're root
+    set -l suffix '❯'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set cwd_color (set_color $fish_color_cwd_root)
+        end
+        set suffix '#'
+    end
+
+    # Color the prompt in red on error
+    if test $last_status -ne 0
+        set status_color (set_color $fish_color_error)
+        set prompt_status $status_color "[" $last_status "]" $normal
+    end
+
+    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -n -s $status_color $suffix ' ' $normal
+end
