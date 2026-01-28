41b31b8b4f7f8f01f272b123da17cf1b80c2d32f
offGustavo <125596752+offGustavo@users.noreply.github.com>
2026-01-28 12:35:44 -0300

    feat(scritps): fix toggle-dark-mode script, make is new scritps and add dms config

--- .local/bin/toggle-dark-mode.sh
+++ .local/bin/toggle-dark-mode.sh
@@ -1,56 +1,31 @@
 #!/usr/bin/env bash
 current_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
 
-kill_if_running() {
-  pids=$(pgrep "$1")
-  if [ -n "$pids" ]; then
-    echo "Found PIDs for $1: $pids"
-    kill $pids
-  else
-    echo "No running process named $1"
-  fi
-}
-
-start_sway() {
-  kill_if_running swaybg 
-  local wallpaper_name="$1"
-  if [[ -n "$wallpaper_name" ]]; then
-    start_swaybg.lua -n "$wallpaper_name"
-  else
-    start_swaybg.lua
-  fi
-}
-
-start_waybar() {
-  if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
-    ln -sf ~/.config/waybar/dark.css ~/.config/waybar/colors.css
-  else
-    ln -sf ~/.config/waybar/light.css ~/.config/waybar/colors.css
-  fi
-   ~/.config/hypr/scripts/toggle-waybar.sh --restart
-}
-
 if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
-
   niri msg action do-screen-transition 
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+  dms ipc call theme dark
 
   ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/colors.ini
   ln -sf ~/.config/rofi/shared/theme/dark.rasi ~/.config/rofi/shared/colors.rasi
   ln -sf ~/.config/hypr/config/theme/dark.conf ~/.config/hypr/config/theme.conf
-  start_waybar
-  start_sway
 
-  # notify-send "Dark Mode" "Switched to dark mode"
+  start_swaybg.lua 
+  $HOME/.config/waybar/scripts/toggle-waybar.sh --restart
+
+  notify-send "Dark Mode" "Switched to dark mode"
+  exit 0
 else
   niri msg action do-screen-transition 
   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+  dms ipc call theme light
 
   ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/colors.ini
   ln -sf ~/.config/rofi/shared/theme/light.rasi ~/.config/rofi/shared/colors.rasi
   ln -sf ~/.config/hypr/config/theme/light.conf ~/.config/hypr/config/theme.conf
-  start_waybar
-  start_sway 
 
-  # notify-send "Light Mode" "Switched to light mode"
+  start_swaybg.lua 
+  $HOME/.config/waybar/scripts/toggle-waybar.sh --restart
+
+  notify-send "Light Mode" "Switched to light mode"
 fi
