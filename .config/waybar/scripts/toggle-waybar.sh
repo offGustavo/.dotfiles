24eedb6346e2e2eb40ff249a08765857fef0f2e5
offGustavo <125596752+offGustavo@users.noreply.github.com>
2026-01-28 12:33:56 -0300

    feat(waybar): fix toggle-waybar script

--- .config/waybar/scripts/toggle-waybar.sh
+++ .config/waybar/scripts/toggle-waybar.sh
@@ -1,14 +1,22 @@
-#/usr/bin/env bash 
+#!/usr/bin/env bash
+
+set -xe
+
+current_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
+
+if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
+    ln -sf ~/.config/waybar/light.css ~/.config/waybar/colors.css
+else
+    ln -sf ~/.config/waybar/dark.css ~/.config/waybar/colors.css
+fi
 
 WAYBAR_PID=$(pgrep waybar)
 if [[ -n "$WAYBAR_PID"  ]]; then
-  kill "$WAYBAR_PID" &
-  if [[ "$1" == "--restart" ]]; then
-    sleep 1
+    kill "$WAYBAR_PID" &
+    if [[ "$1" == "--restart" ]]; then
+	sleep 1
+	waybar &
+    fi
+else
     waybar &
-  fi 
-  exit 0
 fi
-
-waybar &
-exit 0
