8f8d8060eccfada4a65933d0a4d8fa3db4db8d2a
offGustavo <125596752+offGustavo@users.noreply.github.com>
2026-02-17 23:49:08 -0300

    chore(scripts): temp change to dms

--- .local/bin/toggle-dark-mode.sh
+++ .local/bin/toggle-dark-mode.sh
@@ -1,31 +1,77 @@
 #!/usr/bin/env bash
-current_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
 
-if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
+mode=$(dms ipc call theme getMode)
+
+case "$mode" in
+  "light") 
   niri msg action do-screen-transition 
-  gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
   dms ipc call theme dark
-
   ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/colors.ini
-  ln -sf ~/.config/rofi/shared/theme/dark.rasi ~/.config/rofi/shared/colors.rasi
   ln -sf ~/.config/hypr/config/theme/dark.conf ~/.config/hypr/config/theme.conf
-
   start_swaybg.lua 
-  $HOME/.config/waybar/scripts/toggle-waybar.sh --restart
-
-  notify-send "Dark Mode" "Switched to dark mode"
-  exit 0
-else
+  ;;
+  "dark") 
   niri msg action do-screen-transition 
-  gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
   dms ipc call theme light
-
   ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/colors.ini
-  ln -sf ~/.config/rofi/shared/theme/light.rasi ~/.config/rofi/shared/colors.rasi
   ln -sf ~/.config/hypr/config/theme/light.conf ~/.config/hypr/config/theme.conf
-
   start_swaybg.lua 
-  $HOME/.config/waybar/scripts/toggle-waybar.sh --restart
+  exit 0
+  ;;
+esac
 
-  notify-send "Light Mode" "Switched to light mode"
-fi
+
+# if [ "$mode" = "'light'" ]; then
+#   niri msg action do-screen-transition 
+#   dms ipc call theme dark
+#   ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/colors.ini
+#   ln -sf ~/.config/hypr/config/theme/dark.conf ~/.config/hypr/config/theme.conf
+#   start_swaybg.lua 
+#   exit 0
+# else
+#   niri msg action do-screen-transition 
+#   dms ipc call theme light
+#   ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/colors.ini
+#   ln -sf ~/.config/hypr/config/theme/light.conf ~/.config/hypr/config/theme.conf
+#   start_swaybg.lua 
+#   exit 0
+# fi
+
+exit 1
+
+
+# if [[ command -v gsettings ]] then;
+# current_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
+#
+# else
+#
+# fi
+#
+# if [ "$current_scheme" = "'prefer-light'" ] || [ "$current_scheme" = "'default'" ]; then
+#   niri msg action do-screen-transition 
+#   gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+#   dms ipc call theme dark
+#
+#   ln -sf ~/.config/fuzzel/dark.ini ~/.config/fuzzel/colors.ini
+#   ln -sf ~/.config/rofi/shared/theme/dark.rasi ~/.config/rofi/shared/colors.rasi
+#   ln -sf ~/.config/hypr/config/theme/dark.conf ~/.config/hypr/config/theme.conf
+#
+#   start_swaybg.lua 
+#   $HOME/.config/waybar/scripts/toggle-waybar.sh --restart
+#
+#   notify-send "Dark Mode" "Switched to dark mode"
+#   exit 0
+# else
+#   niri msg action do-screen-transition 
+#   gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+#   dms ipc call theme light
+#
+#   ln -sf ~/.config/fuzzel/light.ini ~/.config/fuzzel/colors.ini
+#   ln -sf ~/.config/rofi/shared/theme/light.rasi ~/.config/rofi/shared/colors.rasi
+#   ln -sf ~/.config/hypr/config/theme/light.conf ~/.config/hypr/config/theme.conf
+#
+#   start_swaybg.lua 
+#   $HOME/.config/waybar/scripts/toggle-waybar.sh --restart
+#
+#   notify-send "Light Mode" "Switched to light mode"
+# fi
